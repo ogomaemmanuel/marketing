56df0e8148c247579a1d676d99ab0119033ff28c
emmanuelogoma <eogoma@safaricom.co.ke>
2026-07-19 16:01:32 +0300

    added campaign deatils fields on GetCampaignByIDView

--- core/src/main/java/com/ogoma/marketing/core/application/campaign/GetCampaignByIDView.java
+++ core/src/main/java/com/ogoma/marketing/core/application/campaign/GetCampaignByIDView.java
@@ -1,10 +1,23 @@
 package com.ogoma.marketing.core.application.campaign;
 
 import com.ogoma.marketing.core.domain.campaigns.CampaignEntity;
+import com.ogoma.marketing.core.domain.campaigns.Channel;
 
-public record GetCampaignByIDView() {
+import java.util.Set;
+import java.util.UUID;
 
-    public GetCampaignByIDView(CampaignEntity campaignEntity){
-        this();
+public record GetCampaignByIDView(
+        UUID id,
+        String name,
+        String description,
+        Set<Channel> channels
+) {
+
+    public GetCampaignByIDView(CampaignEntity campaignEntity) {
+        this(
+                campaignEntity.getId().id(),
+                campaignEntity.getName(),
+                campaignEntity.getDescription(),
+                campaignEntity.getChannels());
     }
 }
