f04f658621cd279aa31e69e1423e433761a23f8b
emmanuelogoma <eogoma@safaricom.co.ke>
2026-07-21 08:44:58 +0300

    added scheduling transactional messages

--- core/src/main/java/com/ogoma/marketing/core/application/transactionalmessages/SendTransactionalEmailCommand.java
+++ core/src/main/java/com/ogoma/marketing/core/application/transactionalmessages/SendTransactionalEmailCommand.java
@@ -4,12 +4,14 @@ package com.ogoma.marketing.core.application.transactionalmessages;
 import com.ogoma.marketing.core.abstractions.Command;
 import com.ogoma.marketing.core.domain.email.EmailTemplateID;
 
+import java.time.ZonedDateTime;
 import java.util.List;
 import java.util.Map;
 
 public record SendTransactionalEmailCommand(
         EmailTemplateID templateID,
         List<String> recipients,
-        Map<String, Object> params
+        Map<String, Object> params,
+        ZonedDateTime scheduledAt
 ) implements Command<Void>, TransactionalNotification {
 }
