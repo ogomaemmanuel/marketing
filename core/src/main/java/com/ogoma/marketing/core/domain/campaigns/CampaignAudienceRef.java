d81ea62e9ae6169f19742c39889a14a999a9c26c
emmanuelogoma <eogoma@safaricom.co.ke>
2026-07-22 21:50:53 +0300

    added campaign audience ref

---
+++ core/src/main/java/com/ogoma/marketing/core/domain/campaigns/CampaignAudienceRef.java
@@ -0,0 +1,14 @@
+package com.ogoma.marketing.core.domain.campaigns;
+
+import com.ogoma.marketing.core.domain.audience.AudienceId;
+import com.ogoma.marketing.core.sharedkernel.CustomAssert;
+import org.springframework.data.relational.core.mapping.Table;
+
+
+@Table("campaign_audience")
+public record CampaignAudienceRef(
+        AudienceId audienceId) {
+    public CampaignAudienceRef {
+        CustomAssert.notNull(audienceId, () -> new IllegalArgumentException("Audience id is required"));
+    }
+}
